2a049c8d56dc0368d125f60ebd66c5e09af56ad5
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-03-19 14:38:45 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
  
-gulp.task('zip', function() {
-    return gulp.src('**/*.*')
-        .pipe(zip('Monet-All.zip'))
-        .pipe(gulp.dest('export'));
+gulp.task('zip', function () {
+    return gulp.src('./**')
+        .pipe(zip('test.zip'))
+        .pipe(gulp.dest('./distribution'));
 });
