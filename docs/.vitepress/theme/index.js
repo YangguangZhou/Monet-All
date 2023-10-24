179eb7fc84504dffd149debad11cc5ef018e4efa
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-10-24 23:30:15 +0800

    update documate

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,15 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
-export default DefaultTheme
+import { h } from 'vue'
+import Documate from '@documate/vue'
+import '@documate/vue/dist/style.css'
+
+export default {
+  ...DefaultTheme,
+  Layout: h(DefaultTheme.Layout, null, {
+    'nav-bar-content-before': () => h(Documate, {
+      endpoint: 'https://hej3t2xt2v.us.aircode.run/upload',
+    }),
+  }),
+}
